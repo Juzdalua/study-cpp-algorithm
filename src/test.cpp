7e166002978489692a1a8b2e35be4a714087fdbc
juzdalua <bearwcw@naver.com>
2024-09-12 23:07:14 +0900

    Update prefixsum

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int a = 10;
-    cout << "Test" << 10 << endl;
+    string dopa = "umzunsik";
+    string dopa2 = dopa.substr(0, 3);
+    reverse(dopa2.begin(), dopa2.end());
+    cout << dopa2 << endl;
+
+    double pi = 3.141592653589793238462643383279;
+    cout << fixed << setprecision(5) << pi;
+
     return 0;
 }
