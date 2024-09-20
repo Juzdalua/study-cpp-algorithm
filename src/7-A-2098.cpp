3b7208f9fefdc13dd497dd34463f723583410648
juzdalua <bearwcw@naver.com>
2024-09-21 01:46:05 +0900

    2098 todo

---
+++ src/7-A-2098.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    TSP
+    1~N번 도시
+    도시 사이에는 길이 있거나 없다.
+    N개의 도시를 모두 거치고 원래 도시로 돌아오는 경로
+    한 번 갔던 도시는 다시 갈 수 없다. (마지막은 예외)
+    i->j 도시간 이동 비용 W[i][j]
+    W[i][j] != W[j][i]
+    W[i][i] = 0
+    i에서 j로 가는 길이 없다면, W[i][j] = 0
+    가장 적은 비용을 구하라
+*/
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    // TODO
+    for (int i = 0; i < n; i++)
+    {
+
+    }
+
+    return 0;
+}
