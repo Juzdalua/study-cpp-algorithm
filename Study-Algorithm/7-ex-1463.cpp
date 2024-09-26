e935e5260ecccfc11f0efaf16dee155376148c4a
juzdalua <bearwcw@naver.com>
2024-09-26 13:42:55 +0900

    Solve 1463 bottomup

--- Study-Algorithm/7-ex-1463.cpp
+++ Study-Algorithm/7-ex-1463.cpp
@@ -46,6 +46,22 @@ int Go(int x)
 	return dp[x];
 }
 
+void Solution()
+{
+	dp[1] = 0;
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+
+		dp[i] = min(dp[i], dp[i - 1] + 1);
+	}
+}
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
